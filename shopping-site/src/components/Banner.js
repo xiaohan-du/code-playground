3cff88c31545c7bba2038d0356c4ec5126c18800
thinkvantagedu <duxiaohan2006@gmail.com>
2020-09-10 22:15:27 +0100

    Change useState to useReducer.

--- shopping-site/src/components/Banner.js
+++ shopping-site/src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
@@ -7,36 +7,44 @@ import './Banner.scss';
 SwiperCore.use([Navigation, Pagination]);
 
 const Banner = () => {
+
     const slides = [];
 
-    const [title, setTitle] = useState([]);
-    const [subtitle, setSubtitle] = useState([]);
-    const [imgUrl, setImgUrl] = useState([]);
-    const [renderBanner, setRenderBanner] = useState(false);
+    const bannerReducer = (state, action) => {
+        switch (action.type) {
+            case 'setTitle':
+                return {
+                    ...state,
+                    title: action.title
+                }
+            case 'setSubtitle':
+                return {
+                    ...state,
+                    subtitle: action.subtitle
+                }
+            case 'setImgUrl':
+                return {
+                    ...state,
+                    imgUrl: action.imgUrl
+                }
+            case 'setRenderBanner':
+                return {
+                    ...state,
+                    renderBanner: action.renderBanner
+                }
+            default:
+                return state
+        }
+    }
 
-    for (let i = 0; i < 3; i += 1) {
-        slides.push(
-            <SwiperSlide key={`slide-${i}`}>
-                {renderBanner ? <div className='banner__img'
-                    style={{ backgroundImage: `url(${imgUrl[i]})` }}>
-                    <div className='center-content banner__img__content'>
-                        <div className='banner__title'>{title[i]}</div>
-                        <div className='banner__subtitle'>{subtitle[i]}</div>
-                        <div>
-                            <button className='btn btn__primary'>Contact us</button>
-                        </div>
-                    </div>
-                </div> : null}
-            </SwiperSlide>
-        )
-    };
+    const [state, dispatch] = useReducer(bannerReducer, { title: [], subtitle: [], imgUrl: [], renderBanner: false });
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const apiUrl = `https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details`;
                 let response = await fetch(apiUrl);
-                if (!response.ok) throw new Error('Test API request failed.');
+                if (!response.ok) throw new Error('API request failed.');
                 response = await response.json();
                 let _title = [];
                 let _subtitle = [];
@@ -44,10 +52,10 @@ const Banner = () => {
                 response.Details.forEach(e => _title.push(e.Title));
                 response.Details.forEach(e => _subtitle.push(e.Subtitle));
                 response.Details.forEach(e => _imgUrl.push(e.ImageUrl));
-                setTitle(_title);
-                setSubtitle(_subtitle);
-                setImgUrl(_imgUrl);
-                setRenderBanner(true);
+                dispatch({ type: 'setTitle', title: _title });
+                dispatch({ type: 'setSubtitle', subtitle: _subtitle });
+                dispatch({ type: 'setImgUrl', imgUrl: _imgUrl });
+                dispatch({ type: 'setRenderBanner', renderBanner: true })
             }
             catch (e) {
                 console.log(e);
@@ -55,7 +63,30 @@ const Banner = () => {
             }
         }
         fetchData();
+
     }, []);
+
+    const storeSlides = () => {
+
+        for (let i = 0; i < 3; i += 1) {
+            slides.push(
+                <SwiperSlide key={`slide-${i}`}>
+                    {state.renderBanner ? <div className='banner__img'
+                        style={{ backgroundImage: `url(${state.imgUrl[i]})` }}>
+                        <div className='center-content banner__img__content'>
+                            <div className='banner__title'>{state.title[i]}</div>
+                            <div className='banner__subtitle'>{state.subtitle[i]}</div>
+                            <div>
+                                <button className='btn btn__primary'>Contact us</button>
+                            </div>
+                        </div>
+                    </div> : null}
+                </SwiperSlide>
+            )
+        };
+    }
+
+    storeSlides();
 
     return (
         <>
