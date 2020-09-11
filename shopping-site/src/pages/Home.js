fe5e35951a55b3ccc35b3bb1ee8d7243feafb228
thinkvantagedu <duxiaohan2006@gmail.com>
2020-09-11 16:10:46 +0100

    Add texttile component, unify some styles.

--- shopping-site/src/pages/Home.js
+++ shopping-site/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Tile from '../components/Tile';
+import Texttile from '../components/Texttile';
 
 const Home = () => {
     return (
         <>
             <Banner />
             <Tile />
+            <Texttile />
         </>
     )
 }
