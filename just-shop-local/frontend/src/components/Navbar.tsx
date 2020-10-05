79fc4c98a669e6a44da1b22f6b7f00712b4629bb
thinkvantagedu <duxiaohan2006@gmail.com>
2020-10-05 11:19:46 +0100

    Add Navbar component.

---
+++ just-shop-local/frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light border-bottom">
+            <a href="#/" className='d-flex justify-content-center align-items-center'>
+                <img className='home-logo' src={require('../images/just_shop_local_logo.png')} alt='logo' />
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
